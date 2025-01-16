e3d0c5d579cc6ef56a9c833f1bb9a51bb515f2e4
monichkirill <kirillmonich13@icloud.com>
2025-01-16 13:46:51 +0300

    registration page add

--- src/main/java/servlet/Registration.java
+++ src/main/java/servlet/Registration.java
@@ -1,4 +1,45 @@
 package servlet;
 
-public class Registration {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/registration")
+public class Registration extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Устанавливаем тип контента
+        resp.setContentType("text/html;charset=UTF-8");
+
+        //Получаем из запроса
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String confirmPassword = req.getParameter("confirmPassword");
+
+        boolean isAddUser = UserRepository.addUser(username, password, confirmPassword);
+
+        //Готовим ответ
+        if (isAddUser) {
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+            req.getRequestDispatcher("/login.html").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/registration.html").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = (String) req.getSession().getAttribute("username");
+        if (username == null) {
+            req.getRequestDispatcher("/registration.html").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/page/todo-list.html").forward(req, resp);
+        }
+    }
 }
--- src/main/java/servlet/UserRepository.java
+++ src/main/java/servlet/UserRepository.java
@@ -1,4 +1,35 @@
 package servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserRepository {
+    private static final Map<String, String> users = new HashMap<>();
+
+    static {
+        users.put("admin", "admin");
+        users.put("user", "user");
+    }
+
+    public static Boolean isValid(String username, String password) {
+        if (username == null || password == null) {
+            return false;
+        }
+        if (users.containsKey(username)) {
+            return users.get(username).equals(password);
+        }
+        return false;
+    }
+
+    public static boolean addUser(String username, String password, String confirmPassword) {
+        if (username == null || password == null || confirmPassword == null) {
+            return false;
+        }
+        if (users.containsKey(username) || !password.equals(confirmPassword)) {
+            return false;
+        } else {
+            users.put(username, password);
+        }
+        return true;
+    }
 }
